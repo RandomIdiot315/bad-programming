4ab5568d8339d4b11cc6a3b559f9669cbe82ae1a
RandomIdiot315 <79676090+RandomIdiot315@users.noreply.github.com>
2023-03-01 17:25:49 -0500

    FINISHED

--- ttb.js
+++ ttb.js
@@ -4,6 +4,7 @@ function binaryAgent(str) {
   let hexValues = []
   let unprocessed = []
   let processed = []
+  const comma = /,/gi
 
 for(let i = 0; i < arr.length; i++) {
 
@@ -18,24 +19,21 @@ else {
 dec += k
 
 }
+
 if(j === 7) {
   
   hexValues.push(dec.toString(16))
 
-}
-console.log(arr[i][j])
-}
-console.log(dec + "dec")
-}
+}}}
 
 for(let i = 0; i < hexValues.length; i++) {
 
 unprocessed.push("0x00" + hexValues[i]) 
 processed.push(String.fromCodePoint(unprocessed[i]))
+   
 }
 
-  return processed.toString();
-}
+const translated = processed.toString()
 
-console.log(binaryAgent("01011001 01101111 00100001 00100000 01001001 00100111 01101101 00100000 01110111 01101111 01110010 01101011 01101001 01101110 01100111 00100000 01110000 01110010 01101111 01110000 01100101 01110010 01101100 01111001 00100001"))
-console.log("\u0000")
+return translated.replace(comma, "")
+}
