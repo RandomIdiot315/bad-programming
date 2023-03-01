5c28fe5adb72a01e48d0ca0200c1f23b377ff147
RandomIdiot315 <79676090+RandomIdiot315@users.noreply.github.com>
2023-03-01 16:54:12 -0500

    Update ttb.js

--- ttb.js
+++ ttb.js
@@ -1,10 +1,10 @@
-//by Jaden(yab0ikai)
-
-
 function binaryAgent(str) {
-  
+   
   let arr = str.split(" ")
-  let translated = []
+  let hexValues = []
+  let unprocessed = []
+  let processed = []
+
 for(let i = 0; i < arr.length; i++) {
 
 let k = 128
@@ -20,25 +20,22 @@ dec += k
 }
 if(j === 7) {
   
-  translated.push(dec.toString(16))
+  hexValues.push(dec.toString(16))
 
 }
 console.log(arr[i][j])
 }
 console.log(dec + "dec")
 }
-for(let i = 0; i < translated.length; i++) {
 
-switch(translated[i].length) {
+for(let i = 0; i < hexValues.length; i++) {
 
-case 1: translated.join()
-
+unprocessed.push("0x00" + hexValues[i]) 
+processed.push(String.fromCodePoint(unprocessed[i]))
 }
 
+  return processed.toString();
 }
 
-  return translated;
-}
-
-console.log(binaryAgent("01011001 01101111 00100001 00100000 01001001 00100111 01101101 00100000 01110111 01101111 01110010 01101011 01101001 01101110 01100111 00100000 01110000 01110010 01101111 01110000 01100101 01110010 01101100 01111001 00100001 \u12353"))
-console.log("\u12354")
+console.log(binaryAgent("01011001 01101111 00100001 00100000 01001001 00100111 01101101 00100000 01110111 01101111 01110010 01101011 01101001 01101110 01100111 00100000 01110000 01110010 01101111 01110000 01100101 01110010 01101100 01111001 00100001"))
+console.log("\u0000")
