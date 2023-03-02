87d00e277c5a912312738a4cd23bc0a8285af437
RandomIdiot315 <79676090+RandomIdiot315@users.noreply.github.com>
2023-03-02 14:13:54 -0500

    fixed it

--- averager.js
+++ averager.js
@@ -2,16 +2,18 @@
 //i don't know how to use reduce lol
 //they should really add a Math.average([array]) function
 
-const average = (arr) => { //anonymous function test
 
-var sum = null //is using null instead of 0 a bad practice? i tried both and they worked
-var x = arr.length
+function average(arr) {
+
+let sum = 0
+let x = arr.length
 for (let i = 0; i != arr.length; i++) {
-sum += arr[i] + 3
+
+sum += arr[i]
 
 };
 
-var result =  sum / x;
+let result =  sum / x;
 
 return result;
 }
